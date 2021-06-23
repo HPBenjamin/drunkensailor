43ea12ccb70efb22dc47df5264d3818ba0132326
HPBenjamin <hptischoff@gmail.com>
2021-06-23 22:30:46 +0200

    first playable version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { GameComponent } from './game/game.component';
+import { StartScreenComponent } from './start-screen/start-screen.component';
+const routes: Routes = [
+  { path: '', component: StartScreenComponent },
+  { path: 'game', component: GameComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/dialog-add-player/dialog-add-player.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+@Component({
+  selector: 'app-dialog-add-player',
+  templateUrl: './dialog-add-player.component.html',
+  styleUrls: ['./dialog-add-player.component.scss']
+})
+export class DialogAddPlayerComponent implements OnInit {
+  name: string = '';
+  constructor(private dialog: MatDialog) { }
+
+  ngOnInit(): void {
+  }
+
+  onNoClick() {
+    
+  }
+}
